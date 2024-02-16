043cac9e50b2a17a3e8f95b3762686b5d97f61e6
Jacob Greenwald <jakelgreenwald@gmail.com>
2024-02-15 20:14:21 -0600

    Test

---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using MonkeyFinder.View;
+using MonkeyFinder.Services;
+
+namespace MonkeyFinder;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton<MonkeyService>();
+		builder.Services.AddSingleton<MonkeysViewModel>();
+		builder.Services.AddSingleton<MonkeyDetailsViewModel>();
+		builder.Services.AddSingleton<DetailsPage>();
+		builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+		builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+		builder.Services.AddSingleton<IMap>(Map.Default);
+
+		return builder.Build();
+	}
+}
